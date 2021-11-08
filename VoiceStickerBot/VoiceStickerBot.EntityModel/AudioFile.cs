a9e11dd20972b8d99424049381f73e2fdb8e454b
fkma13552 <ozonplay123@gmail.com>
2021-11-08 09:45:52 +0200

    model and migration

---
+++ VoiceStickerBot/VoiceStickerBot.EntityModel/AudioFile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VoiceStickerBot.EntityModel
+{
+    public class AudioFile
+    {
+        public Guid Id { get; set; }
+        public Guid TelegramFileId { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedOn { get; set; }
+    }
+}
---
+++ VoiceStickerBot/VoiceStickerBot.EntityModel/AudioFileTag.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace VoiceStickerBot.EntityModel
+{
+    public class AudioFileTag
+    {
+        public Guid Id { get; set; }
+        public Guid AudioFileId { get; set; }
+        public Guid TagId { get; set; }
+        
+        public AudioFile AudioFile { get; set; }
+        public Tag Tag { get; set; }
+    }
+}
