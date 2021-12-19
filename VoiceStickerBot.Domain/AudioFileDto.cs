1ad54b2560a8c41bdd28b8181959ae1518148372
Aristokrat <olrpgaristokrat@gmail.com>
2021-12-19 13:11:11 +0200

    Fix get methods

--- VoiceStickerBot.Domain/AudioFileDto.cs
+++ VoiceStickerBot.Domain/AudioFileDto.cs
@@ -5,7 +5,7 @@ namespace VoiceStickerBot.Domain
     public class AudioFileDto
     {
         public Guid Id { get; set; }
-        public Guid TelegramFileId { get; set; }
+        public string TelegramFileId { get; set; }
         public string Name { get; set; }
         public DateTime CreatedOn { get; set; }
     }
